17574e41b7331237f566b5328ea5c6095c849697
coder-tejas <tejashvagupta.x.a@gmail.com>
2026-07-25 23:44:25 +0530

    add pino structured logging for all server-side code, create lib/logger.ts

--- inngest/client.ts
+++ inngest/client.ts
@@ -1,7 +1,5 @@
 import { Inngest } from "inngest";
 
-// console.log("process.env.INNGEST_EVENT_KEY->  ",process.env.INNGEST_EVENT_KEY);
-
 export const inngest = new Inngest({ 
     id: "AgriBoost", 
     isDev: true,
